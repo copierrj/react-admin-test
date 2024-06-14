9bf11d38c33cce2de8a0f67f4577a1eae2b887d0
Reijer Copier <reijer.copier@idgis.nl>
2024-06-14 10:30:37 +0200

    Add UserList

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,10 @@ import {
 } from "react-admin";
 import { dataProvider } from "./dataProvider";
 import { authProvider } from "./authProvider";
+import { UserList } from "./UserList";
 
 export const App = () => (
-  <Admin dataProvider={dataProvider} authProvider={authProvider}></Admin>
+  <Admin dataProvider={dataProvider} authProvider={authProvider}>
+    <Resource name="users" list={UserList} />
+  </Admin>
 );
