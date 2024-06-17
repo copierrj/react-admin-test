63977caf39151a4e1194e8f98659e053a726005a
Reijer Copier <reijer.copier@idgis.nl>
2024-06-17 11:09:33 +0200

    Use resource embedding for Film

--- src/FilmList.tsx
+++ src/FilmList.tsx
@@ -1,16 +1,28 @@
-import { Datagrid, List, NumberField, ReferenceField, TextField } from 'react-admin';
+import { Datagrid, List, NumberField, TextField } from 'react-admin';
 
-export const FilmList = () => (
-    <List>
-        <Datagrid rowClick="show">
-            <TextField source="title" />
-            <TextField source="description" />
-            <NumberField source="release_year" options={{useGrouping: false}}/>
-            <ReferenceField source="language_id" reference="language" >
-                <TextField source="name"/>
-            </ReferenceField>
-            <NumberField source="length" />
-            <TextField source="rating" />
-        </Datagrid>
-    </List>
-);
+export const FilmList = () => {
+    const meta = {
+        columns: [
+            "film_id",
+            "title",
+            "description",
+            "release_year",
+            "...language!film_language_id_fkey(language_name:name)",
+            "length",
+            "rating"
+        ]
+    };
+
+    return (
+        <List queryOptions={{ meta }}>
+            <Datagrid rowClick="show">
+                <TextField source="title" />
+                <TextField source="description" />
+                <NumberField source="release_year" options={{useGrouping: false}}/>
+                <TextField source="language_name"/>
+                <NumberField source="length" />
+                <TextField source="rating" />
+            </Datagrid>
+        </List>
+    );
+};
--- src/FilmShow.tsx
+++ src/FilmShow.tsx
@@ -1,18 +1,30 @@
-import { NumberField, ReferenceField, Show, SimpleShowLayout, TextField } from 'react-admin';
+import { NumberField, Show, SimpleShowLayout, TextField } from 'react-admin';
 import ChipArrayField from './ChipArrayField';
 
-export const FilmShow = () => (
-    <Show>
-        <SimpleShowLayout>
-            <TextField source="title" />
-            <TextField source="description" />
-            <NumberField source="release_year" options={{useGrouping: false}}/>
-            <ReferenceField source="language_id" reference="language" >
-                <TextField source="name"/>
-            </ReferenceField>
-            <NumberField source="length" />
-            <TextField source="rating" />
-            <ChipArrayField source="special_features"/>
-        </SimpleShowLayout>
-    </Show>
-);
+export const FilmShow = () => {
+    const meta = {
+        columns: [
+            "title",
+            "description",
+            "release_year",
+            "...language!film_language_id_fkey(language_name:name)",
+            "length",
+            "rating",
+            "special_features"
+        ]
+    }
+
+    return (
+        <Show queryOptions={{ meta }}>
+            <SimpleShowLayout>
+                <TextField source="title" />
+                <TextField source="description" />
+                <NumberField source="release_year" options={{useGrouping: false}}/>
+                <TextField source="language_name"/>
+                <NumberField source="length" />
+                <TextField source="rating" />
+                <ChipArrayField source="special_features"/>
+            </SimpleShowLayout>
+        </Show>
+    );
+}
