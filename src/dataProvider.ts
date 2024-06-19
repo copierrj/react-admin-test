4161ffeb0c76dff5048156e5ae3f8dcf5b7175d6
Reijer Copier <reijer.copier@idgis.nl>
2024-06-19 11:08:24 +0200

    Add dataprovider logging

--- src/dataProvider.ts
+++ src/dataProvider.ts
@@ -1,4 +1,4 @@
-import { fetchUtils } from 'react-admin';
+import { CreateParams, DeleteManyParams, DeleteParams, GetListParams, GetManyParams, GetManyReferenceParams, GetOneParams, UpdateManyParams, UpdateParams, fetchUtils } from 'react-admin';
 import postgrestRestProvider,
      { IDataProviderConfig,
        defaultSchema } from '@raphiniert/ra-data-postgrest';
@@ -15,9 +15,49 @@ const config: IDataProviderConfig = {
       ["language", ["language_id"]],
       ["customer", ["customer_id"]],
       ["address", ["address_id"]],
-      ["city", ["city_id"]]
+      ["city", ["city_id"]],
+      ["category", ["category_id"]]
     ]),
     schema: defaultSchema
 }
 
-export const dataProvider = postgrestRestProvider(config);
+const postgrestRestDataProvider = postgrestRestProvider(config);
+
+export const dataProvider = {
+  create: (resource: string, params: CreateParams<any>) => {
+    console.log("create", resource, params);
+    return postgrestRestDataProvider.create(resource, params);
+  },
+  delete: (resource: string, params: DeleteParams<any>) => {
+    console.log("delete", resource, params);
+    return postgrestRestDataProvider.delete(resource, params);
+  },
+  deleteMany: (resource: string, params: DeleteManyParams<any>) => {
+    console.log("deleteMany", resource, params);
+    return postgrestRestDataProvider.deleteMany(resource, params);
+  },
+  getList: (resource: string, params: GetListParams) => {
+    console.log("getList", resource, params);
+    return postgrestRestDataProvider.getList(resource, params);
+  },
+  getMany: (resource: string, params: GetManyParams) => {
+    console.log("getMany", resource, params);
+    return postgrestRestDataProvider.getMany(resource, params);
+  },
+  getManyReference: (resource: string, params: GetManyReferenceParams) => {
+    console.log("getManyReference", resource, params);
+    return postgrestRestDataProvider.getManyReference(resource, params);
+  },
+  getOne: (resource: string, params: GetOneParams<any>) => {
+    console.log("getOne", resource, params);
+    return postgrestRestDataProvider.getOne(resource, params);
+  },
+  update: (resource: string, params: UpdateParams<any>) => {
+    console.log("update", resource, params);
+    return postgrestRestDataProvider.update(resource, params);
+  },
+  updateMany: (resource: string, params: UpdateManyParams<any>) => {
+    console.log("updateMany", resource, params);
+    return postgrestRestDataProvider.updateMany(resource, params);
+  }
+};
