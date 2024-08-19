88824a9fdfa67c957ca1a163731fecea49188c74
Reijer Copier <reijer.copier@idgis.nl>
2024-08-19 16:48:21 +0200

    Customize theme

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import {
   Admin,
   Layout,
   Resource,
+  defaultTheme,
   ListGuesser,
   EditGuesser,
   ShowGuesser,
@@ -27,8 +28,44 @@ const MyLayout  = (props: any) => {
   )
 }
 
+const myTheme = {
+  ...defaultTheme,
+  components: {
+    ...defaultTheme.components,
+    MuiAppBar: {
+      defaultProps: {
+        variant: "regular"
+      },
+      styleOverrides: {
+        root: {
+          background: "linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%)"
+        }
+      }
+    },
+    MuiChip: {
+      defaultProps: {
+        variant: "outlined"
+      },
+      styleOverrides: {
+        root: {
+          borderRadius: "10px"
+        },
+        label: {
+          paddingRight: "100px",
+          paddingLeft: "100px"
+        }
+      }
+    },
+    MuiButton: {
+      defaultProps: {
+        variant: "outlined"
+      }
+    }
+  }
+}
+
 export const App = () => (
-  <Admin dataProvider={dataProvider} authProvider={authProvider} layout={MyLayout}>
+  <Admin theme={myTheme} dataProvider={dataProvider} authProvider={authProvider} layout={MyLayout}>
     <Resource name="film" list={FilmList} show={FilmShow} edit={FilmEdit}/>
     <Resource name="actor" list={ActorList} show={ActorShow}/>
     <Resource name="customer" list={CustomerList} show={CustomerShow} edit={CustomerEdit}/>
